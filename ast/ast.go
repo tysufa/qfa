3fded2ac66c64858146c516c6eab7ecdf10c3db7
tysufa <philemon.penot@gmail.com>
2024-03-07 23:26:49 +0100

    added abstract syntax tree and started working on the parser

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import (
+	"github.com/tysufa/qfa/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Value Expression
+	Token token.Token
+	Name  *Identifier
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
---
+++ parser/parser.go
@@ -0,0 +1,10 @@
+package parser
+
+import (
+	"github.com/tysufa/qfa/lexer"
+	"github.com/tysufa/qfa/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+}
