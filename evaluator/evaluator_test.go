763f6508caa7a7063e8f15f5aab857f7532171b0
tysufa <philemon.penot@gmail.com>
2024-04-26 19:04:41 +0200

    evaluation of prefix expressions complete

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,6 +7,24 @@ import (
 	"testing"
 )
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func TestEvalBooleanExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -41,6 +59,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
